b6fb75d879fc3130ae3e865b7715cd11c760f3cb
Remi CLERMONT (contractor) <Remi.CLERMONT@amadeus.com>
2018-06-05 14:33:27 +0400

    Training exercice implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public posts = [new Post('Titi', 'Test', 0), new Post('Tata', 'Test', 0), new Post('Tutu', 'Test', 0)];
 }
